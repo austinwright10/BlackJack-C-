6ed0713c4f8cce3d78b9adf6fe2c32a827c7a354
austinwright10 <austwright10@gmail.com>
2024-01-23 20:58:14 -0700

    finished the card function and got started on the player function/player's hand in the game

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -1,23 +1,41 @@
-//
-//  main.cpp
-//  BlackJack
-//
-//  Created by Austin Wright on 1/21/24.
-//
-
 #include <iostream>
 #include <string>
+#include <random>
+#include <vector>
+
 using namespace std;
 
-void player();
+int card() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distribution(0, 21);
+    int random_number = distribution(gen);
+    return random_number;
+}
+
+vector<int> player() {
+    vector<int> nums = {};
+    int i = 1;
+    
+    do {
+        int cardResult = card();
+        nums.push_back(cardResult);
+        i++;
+    } while (i < 3);
+    
+
+    
+    return nums;
+}
 void dealer();
 void winner();
 void run_game();
 
-
 int main() {
     
     char startGame;
+    int cardResult = card();
+    vector<int> player_hand = player();
     
     cout << "Welcome to BlackJack!" << endl;
     
@@ -25,7 +43,12 @@ int main() {
     cin.get(startGame);
     
     if (startGame == '\n') {
-        cout << "worked" << endl;
+        cout << "Card generated: " << cardResult << endl;
+        cout << "Player's hand generated " << endl;
+        for (int card: player_hand) {
+            cout << card << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
