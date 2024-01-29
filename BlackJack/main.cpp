cc1cb5f0cace44e6de2ee6e62620391544fa8b97
austinwright10 <austwright10@gmail.com>
2024-01-29 15:01:16 -0700

    added dealers hand and some game functionality

--- BlackJack/main.cpp
+++ BlackJack/main.cpp
@@ -6,14 +6,16 @@
 using namespace std;
 
 int card() {
+    // card function, randomly selects a number from 0 - 11
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> distribution(0, 21);
+    std::uniform_int_distribution<int> distribution(0, 11);
     int random_number = distribution(gen);
     return random_number;
 }
 
 vector<int> player() {
+    // randomly selects two card values for the player
     vector<int> nums = {};
     int i = 1;
     
@@ -23,32 +25,53 @@ vector<int> player() {
         i++;
     } while (i < 3);
     
+    return nums;
+}
 
+vector<int> dealer() {
+    // randomly selects two card values for the dealer
+    vector<int> nums = {};
+    int i = 1;
+    
+    do {
+        int cardResult = card();
+        nums.push_back(cardResult);
+        i++;
+    } while (i < 3);
     
     return nums;
 }
-void dealer();
 void winner();
 void run_game();
 
 int main() {
-    
     char startGame;
+    int play_counter = 0;
     int cardResult = card();
     vector<int> player_hand = player();
-    
+    vector<int> dealer_hand = dealer();
+
     cout << "Welcome to BlackJack!" << endl;
-    
+
     cout << "Press 'Enter' to start the game." << endl;
     cin.get(startGame);
-    
-    if (startGame == '\n') {
-        cout << "Card generated: " << cardResult << endl;
-        cout << "Player's hand generated " << endl;
-        for (int card: player_hand) {
-            cout << card << " ";
+
+    while (play_counter < 2) {
+        if (startGame == '\n') {
+            cout << "Player's hand: ";
+            for (int card : player_hand) {
+                cout << card << " ";
+            }
+            cout << endl;
+
+            cout << "Dealer's hand: ";
+            for (int card : dealer_hand) {
+                cout << card << " ";
+            }
+            cout << endl;
+
+            play_counter++;
         }
-        cout << endl;
     }
     return 0;
 }
